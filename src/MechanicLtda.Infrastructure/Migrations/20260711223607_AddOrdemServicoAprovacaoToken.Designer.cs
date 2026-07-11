4586138f5d2d7643728844e4c531574abbfdd03e
lucas <lucassiqueiranadir@gmail.com>
2026-07-11 19:38:54 -0300

    Corrige sql server no docker

--- src/MechanicLtda.Infrastructure/Migrations/20260711223607_AddOrdemServicoAprovacaoToken.Designer.cs
+++ src/MechanicLtda.Infrastructure/Migrations/20260711223607_AddOrdemServicoAprovacaoToken.Designer.cs
@@ -4,6 +4,7 @@ using MechanicLtda.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MechanicLtda.Infrastructure.Migrations
 {
     [DbContext(typeof(BancoAPIContext))]
-    partial class BancoAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20260711223607_AddOrdemServicoAprovacaoToken")]
+    partial class AddOrdemServicoAprovacaoToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/MechanicLtda.Infrastructure/Migrations/20260711223607_AddOrdemServicoAprovacaoToken.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MechanicLtda.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrdemServicoAprovacaoToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrdemServicoAprovacaoTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrdemServicoId = table.Column<int>(type: "int", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataExpiracao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Utilizado = table.Column<bool>(type: "bit", nullable: false),
+                    DataUtilizacao = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrdemServicoAprovacaoTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrdemServicoAprovacaoTokens_OrdensServico_OrdemServicoId",
+                        column: x => x.OrdemServicoId,
+                        principalTable: "OrdensServico",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrdemServicoAprovacaoTokens_OrdemServicoId",
+                table: "OrdemServicoAprovacaoTokens",
+                column: "OrdemServicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrdemServicoAprovacaoTokens_Token",
+                table: "OrdemServicoAprovacaoTokens",
+                column: "Token",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrdemServicoAprovacaoTokens");
+        }
+    }
+}
